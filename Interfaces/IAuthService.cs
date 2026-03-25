af8ad1e21f369e0e2ee4b821ea8a6bcd207ea305
Mohamed Aziz Hafhouf <mohamedazizhafhouf7@gmail.com>
2026-03-25 09:28:16 +0100

    feat(auth): prearing DTOs, Interfaces and Service

---
+++ Interfaces/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using FreelancerManagementSystem.Models;
+using FreelancerManagementSystem.DTOs;
+
+namespace FreelancerManagementSystem.Interfaces
+{
+    public class IAuthService
+    {
+        Task<User?> Register(RegisterDto request);
+        Task<string?> Login(LoginDto request); // Returns jwt or null
+    }
+}
